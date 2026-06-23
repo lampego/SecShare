a14742c7e84f5d65501ee018bb4155e0b04d134f
lampego <lampego@gmail.com>
2026-06-23 19:38:13 +0300

    PR-10480 Added Console project

---
+++ SecShare/SecShare.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Progress()
+    .Columns(
+        new TaskDescriptionColumn(),
+        new ProgressBarColumn(),
+        new DownloadedColumn(),
+        new TransferSpeedColumn(),
+        new RemainingTimeColumn())
+    .Start(ctx =>
+    {
+        var task = ctx.AddTask("game-installer.exe", maxValue: 524288000); // 500 MB in bytes
+  
+        while (!ctx.IsFinished)
+        {
+            task.Increment(2621440); // 2.5 MB per tick
+            Thread.Sleep(50);
+        }
+    });
